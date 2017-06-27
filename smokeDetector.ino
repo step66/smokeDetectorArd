1c816ab222557809eff2a7dd12d87a7d1a826066
step66 <red.mailhub@gmail.com>
2017-06-27 13:36:26 +0800

    smokeDet-cam-noPIR

--- smokeDetector.ino
+++ smokeDetector.ino
@@ -1,69 +1,245 @@
+#include <SPI.h>
+#include <SD.h>
 
-#include <Wire.h>
-#include <UnoWiFiDevEd.h>
- 
-#define CONNECTOR     "rest" 
-#define SERVER_ADDR   "api.thingspeak.com"
- 
-#define APIKEY_THINGSPEAK  "PB8IZ8SJLS33EW2C" //Insert your API Key 
- 
-short hum = 60;
-short temp = 22;
-short delta;
+/*macro definitions of PIR motion sensor pin and LED pin*/
+#define PIR_MOTION_SENSOR 2//Use pin 2 to receive the signal from the module
+
+
 
 // smoke detector
-
 #define redLed 6
 #define detector A0
 
 short reading1;
 short reading2;
+short delta;
 
-void setup() {
-  Serial.begin(9600);
-  Ciao.begin(); // CIAO INIT
-  pinMode(redLed, OUTPUT);   
-  pinMode(detector, INPUT);  
-}
- 
-void loop() {
+#define PIC_PKT_LEN    128                  //data length of each read, dont set this too big because ram is limited
+#define PIC_FMT_VGA    7
+#define PIC_FMT_CIF    5
+#define PIC_FMT_OCIF   3
+#define CAM_ADDR       0
+#define CAM_SERIAL     Serial
 
-  reading1 = analogRead(detector);
-  delay(500);
-  reading2 = analogRead(detector);
-  delta = abs(reading1 - reading2);
+#define PIC_FMT        PIC_FMT_VGA
 
-  if (delta > 2){
+File myFile;
+
+const byte cameraAddr = (CAM_ADDR << 5);  // addr
+const int buttonPin = A5;                 // the number of the pushbutton pin
+unsigned long picTotalLen = 0;            // picture length
+int picNameNum = 0;
+
+/*********************************************************************/
+void setup()
+{
+    Serial.begin(115200);
     
-    digitalWrite(redLed, HIGH);
-    Serial.println(delta);
-     
-    String uri = "/update?api_key=";
-    uri += APIKEY_THINGSPEAK;
-    uri += "&field1=";
-    uri += String(hum);
-    uri += "&field2=";
-    uri += String(temp);
-    uri += "&field3=";
-    uri += String(delta);
- 
-    Ciao.println("Send data on ThingSpeak Channel"); 
-      
-    CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri);
- 
-    if (!data.isEmpty()){
-      Ciao.println( "State: " + String (data.get(1)) );
-      Ciao.println( "Response: " + String (data.get(2)) );
+    pinMode(PIR_MOTION_SENSOR, INPUT);
+
+
+    pinMode(redLed, OUTPUT);   
+    pinMode(detector, INPUT);  
+  
+    Serial.println("Initializing SD card....");
+    pinMode(10,OUTPUT);          // CS pin of SD Card Shield
+
+    if (!SD.begin(4)) 
+    {
+        Serial.print("sd init failed");
+        return;
     }
-    else { 
-      Ciao.println("Write Error");
-    }    
-    
-  delay(30000); // Thinkspeak policy
-  } else {
-    digitalWrite(redLed, LOW);
-    Serial.println(reading1);
-    Serial.println(reading2);
-    Serial.println("nothing");
-  }
+    Serial.println("sd init done.");
+    initialize();
+}
+/*********************************************************************/
+void loop()
+{
+    int n = 0;
+    while(1){
+//        Serial.println("\r\nReading environemnt");
+
+        reading1 = analogRead(detector);
+        delay(500);
+        reading2 = analogRead(detector);
+        delta = abs(reading1 - reading2);
+
+
+        if(delta > 4){
+          digitalWrite(redLed, HIGH);
+//          Serial.println(delta);
+//          Serial.println("\r\nbegin to take picture");
+          delay(200);
+
+          if (n == 0) preCapture();
+          Capture();
+          GetData();        
+//          Serial.print("\r\nTaking pictures success ,number : ");
+//          Serial.println(n);
+          n++ ;
+          delay(5000);
+          digitalWrite(redLed, LOW);
+        }
+    }
+}
+/*********************************************************************/
+void clearRxBuf()
+{
+    while (Serial.available())
+    {
+        Serial.read();
+    }
+}
+/*********************************************************************/
+void sendCmd(char cmd[], int cmd_len)
+{
+    for (char i = 0; i < cmd_len; i++) Serial.print(cmd[i]);
+}
+/*********************************************************************/
+void initialize()
+{
+    char cmd[] = {0xaa,0x0d|cameraAddr,0x00,0x00,0x00,0x00} ;
+    unsigned char resp[6];
+
+    Serial.setTimeout(500);
+    while (1)
+    {
+        //clearRxBuf();
+        sendCmd(cmd,6);
+        if (Serial.readBytes((char *)resp, 6) != 6)
+        {
+            continue;
+        }
+        if (resp[0] == 0xaa && resp[1] == (0x0e | cameraAddr) && resp[2] == 0x0d && resp[4] == 0 && resp[5] == 0)
+        {
+            if (Serial.readBytes((char *)resp, 6) != 6) continue;
+            if (resp[0] == 0xaa && resp[1] == (0x0d | cameraAddr) && resp[2] == 0 && resp[3] == 0 && resp[4] == 0 && resp[5] == 0) break;
+        }
+    }
+    cmd[1] = 0x0e | cameraAddr;
+    cmd[2] = 0x0d;
+    sendCmd(cmd, 6);
+    Serial.println("\nCamera initialization done.");
+}
+/*********************************************************************/
+void preCapture()
+{
+    char cmd[] = { 0xaa, 0x01 | cameraAddr, 0x00, 0x07, 0x00, PIC_FMT };
+    unsigned char resp[6];
+
+    Serial.setTimeout(100);
+    while (1)
+    {
+        clearRxBuf();
+        sendCmd(cmd, 6);
+        if (Serial.readBytes((char *)resp, 6) != 6) continue;
+        if (resp[0] == 0xaa && resp[1] == (0x0e | cameraAddr) && resp[2] == 0x01 && resp[4] == 0 && resp[5] == 0) break;
+    }
+}
+void Capture()
+{
+    char cmd[] = { 0xaa, 0x06 | cameraAddr, 0x08, PIC_PKT_LEN & 0xff, (PIC_PKT_LEN>>8) & 0xff ,0};
+    unsigned char resp[6];
+
+    Serial.setTimeout(100);
+    while (1)
+    {
+        clearRxBuf();
+        sendCmd(cmd, 6);
+        if (Serial.readBytes((char *)resp, 6) != 6) continue;
+        if (resp[0] == 0xaa && resp[1] == (0x0e | cameraAddr) && resp[2] == 0x06 && resp[4] == 0 && resp[5] == 0) break;
+    }
+    cmd[1] = 0x05 | cameraAddr;
+    cmd[2] = 0;
+    cmd[3] = 0;
+    cmd[4] = 0;
+    cmd[5] = 0;
+    while (1)
+    {
+        clearRxBuf();
+        sendCmd(cmd, 6);
+        if (Serial.readBytes((char *)resp, 6) != 6) continue;
+        if (resp[0] == 0xaa && resp[1] == (0x0e | cameraAddr) && resp[2] == 0x05 && resp[4] == 0 && resp[5] == 0) break;
+    }
+    cmd[1] = 0x04 | cameraAddr;
+    cmd[2] = 0x1;
+    while (1)
+    {
+        clearRxBuf();
+        sendCmd(cmd, 6);
+        if (Serial.readBytes((char *)resp, 6) != 6) continue;
+        if (resp[0] == 0xaa && resp[1] == (0x0e | cameraAddr) && resp[2] == 0x04 && resp[4] == 0 && resp[5] == 0)
+        {
+            Serial.setTimeout(1000);
+            if (Serial.readBytes((char *)resp, 6) != 6)
+            {
+                continue;
+            }
+            if (resp[0] == 0xaa && resp[1] == (0x0a | cameraAddr) && resp[2] == 0x01)
+            {
+                picTotalLen = (resp[3]) | (resp[4] << 8) | (resp[5] << 16);
+                Serial.print("picTotalLen:");
+                Serial.println(picTotalLen);
+                break;
+            }
+        }
+    }
+
+}
+/*********************************************************************/
+void GetData()
+{
+    unsigned int pktCnt = (picTotalLen) / (PIC_PKT_LEN - 6);
+    if ((picTotalLen % (PIC_PKT_LEN-6)) != 0) pktCnt += 1;
+
+    char cmd[] = { 0xaa, 0x0e | cameraAddr, 0x00, 0x00, 0x00, 0x00 };
+    unsigned char pkt[PIC_PKT_LEN];
+
+    char picName[] = "pic00.jpg";
+    picName[3] = picNameNum/10 + '0';
+    picName[4] = picNameNum%10 + '0';
+
+    if (SD.exists(picName))
+    {
+        SD.remove(picName);
+    }
+
+    myFile = SD.open(picName, FILE_WRITE);
+    if(!myFile){
+        Serial.println("myFile open fail...");
+    }
+    else{
+        Serial.setTimeout(1000);
+        for (unsigned int i = 0; i < pktCnt; i++)
+        {
+            cmd[4] = i & 0xff;
+            cmd[5] = (i >> 8) & 0xff;
+
+            int retry_cnt = 0;
+            retry:
+            delay(10);
+            clearRxBuf();
+            sendCmd(cmd, 6);
+            uint16_t cnt = Serial.readBytes((char *)pkt, PIC_PKT_LEN);
+
+            unsigned char sum = 0;
+            for (int y = 0; y < cnt - 2; y++)
+            {
+                sum += pkt[y];
+            }
+            if (sum != pkt[cnt-2])
+            {
+                if (++retry_cnt < 100) goto retry;
+                else break;
+            }
+
+            myFile.write((const uint8_t *)&pkt[4], cnt-6);
+            //if (cnt != PIC_PKT_LEN) break;
+        }
+        cmd[4] = 0xf0;
+        cmd[5] = 0xf0;
+        sendCmd(cmd, 6);
+    }
+    myFile.close();
+    picNameNum ++;
 }
